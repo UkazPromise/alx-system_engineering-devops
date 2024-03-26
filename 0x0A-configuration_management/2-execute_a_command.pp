7fcee5bd43c87ccdfa4109e78deb1a8e73f97575
UkazPromise <ukazpromise@gmail.com>
2024-03-25 18:14:20 -0700

    2-execute_a_command.pp update

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
-exec { 'killmenow':
-  command     => '/usr/bin/pkill killmenow',
-  path        => ['/bin', '/usr/bin'],
-  onlyif      => '/usr/bin/pgrep -f killmenow',
-  refreshonly => true,
+# kill process killmenow
+
+exec { 'pkill':
+  command     => 'pkill killmenow',
+  provider    => 'shell',
 }
